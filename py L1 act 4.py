78d83f2090fb355813d26956f8b1f10647c719e0
yushra0 <zayaanyushra@gmail.com>
2025-06-19 23:39:43 +0600

    Add files via upload

---
+++ py L1 act 4.py
@@ -0,0 +1,13 @@
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Print all the keywords present in Python
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
